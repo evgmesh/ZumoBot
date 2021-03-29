675221ca61e5718f3da963e5680e01b4ee348e87
Dongbin Yang <dongbin.yang@metropolia.fi>
2021-03-29 14:47:01 +0300

    add dongbin's codes

---
+++ Dongbin.h
@@ -0,0 +1,27 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include <stdint.h>
+
+#ifndef DONGBIN_H
+#define DONGBIN_H
+    
+void progEnd(uint32_t delay);
+void tankTL(uint8_t speed, uint32_t delay);
+void tankTR(uint8_t speed, uint32_t delay);
+void softTR();
+    
+    
+    
+#endif
+
+
+/* [] END OF FILE */
